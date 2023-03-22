8ea3e64cf60e8b55980d4c21f7e45cc44f1f6d07
Renata Dellamatriz <r.dellamatriz@hotmail.com>
2023-03-22 14:22:16 -0300

    feat: fetch github user data

--- src/pages/Home/components/ProfileInfo/index.tsx
+++ src/pages/Home/components/ProfileInfo/index.tsx
@@ -7,15 +7,58 @@ import {
 } from './styles'
 import { BsGithub, BsFillBuildingFill } from 'react-icons/bs'
 import { FaUserFriends, FaExternalLinkAlt } from 'react-icons/fa'
+import { useCallback, useEffect, useState } from 'react'
+import { api } from '../../../../lib/axios'
+
+interface UserProps {
+  name: string
+  username: string
+  imgUrl: string
+  url: string
+  description: string
+  company: string
+  followers: number
+}
 
 export function ProfileInfo() {
+  const [user, setUser] = useState<UserProps>()
+
+  const fetchUserData = useCallback(async () => {
+    const response = await api.get(`users/renatadellamatriz`)
+    const {
+      name,
+      avatar_url: avatarUrl,
+      html_url: htmlUrl,
+      company,
+      followers,
+      bio,
+      login,
+    } = response.data
+
+    const newUser = {
+      name,
+      imgUrl: avatarUrl,
+      url: htmlUrl,
+      company,
+      followers,
+      description: bio,
+      username: login,
+    }
+    setUser(newUser)
+  }, [])
+
+  useEffect(() => {
+    fetchUserData()
+  }, [fetchUserData])
+  console.log(user)
+
   return (
     <ProfileContainer>
-      <img src="https://github.com/renatadellamatriz.png" alt="profile_image" />
+      <img src={user?.imgUrl} alt="profile_image" />
       <div>
         <HeaderProfile>
-          <Title>Renata Dellamatriz</Title>
-          <a href="https://github.com/renatadellamatriz">
+          <Title>{user?.name}</Title>
+          <a href={`https://github.com/${user?.username}`}>
             Github <FaExternalLinkAlt />
           </a>
         </HeaderProfile>
@@ -28,15 +71,15 @@ export function ProfileInfo() {
         <Footer>
           <div>
             <BsGithub size={18} />
-            <span>renatadellmatriz</span>
+            <span>{user?.username}</span>
           </div>
           <div>
             <BsFillBuildingFill size={18} />
-            <span>Sítio Digital</span>
+            <span>{user?.company}</span>
           </div>
           <div>
             <FaUserFriends size={18} />
-            <span>12 followers</span>
+            <span>{user?.followers} followers</span>
           </div>
         </Footer>
       </div>
